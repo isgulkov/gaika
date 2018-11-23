e2e47c1c4400a6eb66a564f2cb33b85b744c603e
Ilya Gulkov <x@isgulkov.ru>
2018-11-23 10:27:28 +0300

    Implement the other regular polyhedra

--- tools/gen_platonic.py
+++ tools/gen_platonic.py
@@ -29,13 +29,163 @@ def tetrahedron(l_edge):
 		(2, 1, 0),
 		(0, 1, 3),
 		(1, 2, 3),
-		(2, 0, 3)
+		(2, 0, 3),
 	)
 
 	return vx, faces
 
+
+def hexahedron(l_edge):
+	vx = (
+		(-l_edge / 2, -l_edge / 2, 0),
+		(-l_edge / 2, l_edge / 2, 0),
+		(l_edge / 2, l_edge / 2, 0),
+		(l_edge / 2, -l_edge / 2, 0),
+		(-l_edge / 2, -l_edge / 2, l_edge),
+		(-l_edge / 2, l_edge / 2, l_edge),
+		(l_edge / 2, l_edge / 2, l_edge),
+		(l_edge / 2, -l_edge / 2, l_edge),
+	)
+
+	faces = (
+		(3, 2, 1, 0),
+		(0, 1, 5, 4),
+		(1, 2, 6, 5),
+		(2, 3, 7, 6),
+		(3, 0, 4, 7),
+		(4, 5, 6, 7)
+	)
+
+	return vx, faces
+
+
+def octahedron(l_edge):
+	# https://en.wikipedia.org/wiki/Octahedron#Cartesian_coordinates
+	coord = l_edge / sqrt(2)
+
+	vx = (
+		(0, 0, -coord),
+		(-coord, 0, 0),
+		(0, coord, 0),
+		(coord, 0, 0),
+		(0, -coord, 0),
+		(0, 0, coord),
+	)
+
+	faces = (
+		(0, 2, 1),
+		(0, 3, 2),
+		(0, 4, 3),
+		(0, 1, 4),
+		(1, 2, 5),
+		(2, 3, 5),
+		(3, 4, 5),
+		(4, 1, 5),
+	)
+
+	return vx, faces
+
+
+def dodecahedron(l_edge):
+	# https://en.wikipedia.org/wiki/Regular_dodecahedron#Cartesian_coordinates
+	phi = (1.0 + sqrt(5)) / 2
+
+	big = phi * l_edge / (2.0 / phi)
+	med = l_edge / (2.0 / phi)
+	small = l_edge / phi / (2.0 / phi)
+
+	vx = (
+		(-small, 0, -big),
+		(small, 0, -big),
+		(-med, -med, -med),
+		(-med, med, -med),
+		(med, med, -med),
+		(med, -med, -med),
+		(0, -big, -small),
+		(0, big, -small),
+		(-big, -small, 0),
+		(-big, small, 0),
+		(big, small, 0),
+		(big, -small, 0),
+		(0, -big, small),
+		(0, big, small),
+		(-med, -med, med),
+		(-med, med, med),
+		(med, med, med),
+		(med, -med, med),
+		(-small, 0, big),
+		(small, 0, big),
+	)
+
+	faces = (
+		(1, 0, 2, 6, 5),
+		(0, 1, 4, 7, 3),
+		(0, 3, 9, 8, 2),
+		(1, 5, 11, 10, 4),
+		(2, 8, 14, 12, 6),
+		(3, 7, 13, 15, 9),
+		(4, 10, 16, 13, 7),
+		(5, 6, 12, 17, 11),
+		(8, 9, 15, 18, 14),
+		(10, 11, 17, 19, 16),
+		(12, 14, 18, 19, 17),
+		(13, 16, 19, 18, 15),
+	)
+
+	return vx, faces
+
+
+def icosahedron(l_edge):
+	# https://en.wikipedia.org/wiki/Regular_icosahedron#Cartesian_coordinates
+	phi = (1.0 + sqrt(5)) / 2
+
+	big = phi * l_edge / 2
+	reg = l_edge / 2.0
+
+	vx = (
+		(0, -reg, -big),
+		(0, reg, -big),
+		(-big, 0, -reg),
+		(big, 0, -reg),
+		(-reg, -big, 0),
+		(-reg, big, 0),
+		(reg, big, 0),
+		(reg, -big, 0),
+		(-big, 0, reg),
+		(big, 0, reg),
+		(0, -reg, big),
+		(0, reg, big),
+	)
+
+	faces = (
+		(1, 0, 3),
+		(0, 1, 2),
+		(0, 4, 7),
+		(1, 6, 5),
+		(0, 2, 4),
+		(1, 5, 2),
+		(1, 3, 6),
+		(0, 7, 3),
+		(2, 8, 4),
+		(2, 5, 8),
+		(3, 9, 6),
+		(3, 7, 9),
+		(4, 10, 7),
+		(5, 6, 11),
+		(7, 10, 9),
+		(4, 8, 10),
+		(5, 11, 8),
+		(6, 9, 11),
+		(9, 10, 11),
+		(8, 11, 10),
+	)
+
+	return vx, faces
+
+
 def norm(xs, ys):
 	return sqrt(sum([(x - y) ** 2 for (x, y) in zip(xs, ys)]))
+
 
 def edge_norms(vx, faces):
 	edges = set()
@@ -46,10 +196,12 @@ def edge_norms(vx, faces):
 
 	return [norm(vx[a], vx[b]) for (a, b) in edges]
 
+
 def stddev(l_expected, ls):
 	return sqrt(sum((l - l_expected) ** 2 for l in ls))
 
-def print_obj(name, vx, faces, l_edge=None):
+
+def print_obj(name, l_edge, vx, faces):
 	print "o {}\n".format(name)
 
 	for x, y, z in vx:
@@ -60,10 +212,11 @@ def print_obj(name, vx, faces, l_edge=None):
 		print 'f', ' '.join([str(v + 1) for v in face_vx])
 	print
 
-	if l_edge is not None:
-		total_err = sum([abs(l - l_edge) for l in edge_norms(vx, faces)])
+	lengths_edges = edge_norms(vx, faces)
+	stderr.write("Total number of edges: \x1b[1m{:d}\x1b[0m.\n".format(len(lengths_edges)))
 
-		stderr.write("Total error in edge lengths: \x1b[1m{:f}\x1b[0m.\n".format(total_err))
+	total_err = sum([abs(l - l_edge) for l in lengths_edges])
+	stderr.write("Total error in edge lengths: \x1b[1m{:f}\x1b[0m.\n".format(total_err))
 
 if __name__ == '__main__':
 	args = parser.parse_args()
@@ -71,13 +224,16 @@ if __name__ == '__main__':
 	n_faces = args.n_faces
 	l_edge = args.l_edge
 
-	if n_faces not in (4, 6, 8, 12, 20):
-		stderr.write("\x1b[1mError\x1b[0m: there are no platonic solids with {} faces\n".format(n_faces))
-		exit(0)
-
 	if n_faces == 4:
-		vx, faces = tetrahedron(l_edge)
-
-		print_obj('Tetrahedron', vx, faces, l_edge=l_edge)
+		print_obj('Tetrahedron', l_edge, *tetrahedron(l_edge))
+	elif n_faces == 6:
+		print_obj('Hexahedron', l_edge, *hexahedron(l_edge))
+	elif n_faces == 8:
+		print_obj('Octahedron', l_edge, *octahedron(l_edge))
+	elif n_faces == 12:
+		print_obj('Dodecahedron', l_edge, *dodecahedron(l_edge))
+	elif n_faces == 20:
+		print_obj('Icosahedron', l_edge, *icosahedron(l_edge))
 	else:
-		print n_faces, l_edge, "!!!"
+		stderr.write("\x1b[1;31mError\x1b[0m: no platonic solids with {} faces exist, LOL\n".format(n_faces))
+		exit(1)
